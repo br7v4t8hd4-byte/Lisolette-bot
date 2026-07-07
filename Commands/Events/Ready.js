67ff9240fc606e70032fd8a8bdee1525e5a970ac
br7v4t8hd4-byte <br7v4t8hd4@privaterelay.appleid.com>
2026-07-07 00:24:51 -0500

    Update Ready.js

--- Commands/Events/Ready.js
+++ Commands/Events/Ready.js
@@ -1,1 +1,9 @@
+const { Events } = require("discord.js");
 
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(`🌸 ${client.user.tag} está en línea.`);
+  },
+};
