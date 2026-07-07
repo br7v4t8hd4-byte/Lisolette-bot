b3e7972bd201fe441849d81c6c46fc4e6127b01e
br7v4t8hd4-byte <br7v4t8hd4@privaterelay.appleid.com>
2026-07-07 00:26:02 -0500

    Update guildMemberAdd.js

--- Commands/Events/guildMemberAdd.js
+++ Commands/Events/guildMemberAdd.js
@@ -1,1 +1,19 @@
+const { Events } = require("discord.js");
 
+module.exports = {
+  name: Events.GuildMemberAdd,
+
+  async execute(member) {
+    const canal = member.guild.systemChannel;
+
+    if (!canal) return;
+
+    canal.send(
+      `🌸 ¡Bienvenido ${member} a **${member.guild.name}**!
+
+Soy **Lisolette**, tu asistente para Mudae.
+
+✨ Usa **/ayuda** para comenzar.`
+    );
+  },
+};
